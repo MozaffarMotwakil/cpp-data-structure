d3a6e3d0ff7a08b8f590d2eb8033b18ac0f8a6e1
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-04-23 14:29:05 +0200

    Added clsDoublyLinkedList

---
+++ level-1/P1-LinkedListClass/P1-LinkedListClass.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "clsDoublyLinkedList.h"
+
+using namespace std;
+
+int main()
+{
+	clsDoublyLinkedList<int> doublyLinkedList;
+
+	// Show clsDoublyLinkedList class in clsDblLinkedList.h file
+
+	return 0;
+}
