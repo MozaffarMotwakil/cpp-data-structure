1d711242d34de1d838540cba88cf2f76270a524b
Mozaffar Motwakil Khider Salman <mozaffarmotwakil@gmail.com>
2025-04-23 17:42:32 +0200

    Added QueueClass

---
+++ level-1/QueueClass/QueueClass.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "clsQueue.h"
+
+using namespace std;
+
+// Show implementation of clsQueue class in clsQueue.h file
+
+int main()
+{
+
+	clsQueue<int> queue;
+
+	queue.push(10);
+	queue.push(20);
+	queue.push(30);
+	queue.push(40);
+	queue.push(50);
+
+	cout << "Queue's List: ";
+	queue.Print();
+
+	cout << "Queue's Size : " << queue.Size() << endl;
+	cout << "Queue's Front: " << queue.front() << endl;
+	cout << "Queue's Back : " << queue.back() << endl;
+
+	queue.pop();
+	cout << "Queue's List after pop: ";
+	queue.Print();
+
+	queue.InsertAtFront(10);
+	cout << "Queue's List after insert 10 at front: ";
+	queue.Print();
+	
+	queue.InsertAtBack(60);
+	cout << "Queue's List after insert 60 at back: ";
+	queue.Print();
+
+	cout << "Item(2): " << queue.GetItem(2) << endl;
+
+	queue.InsertAfter(2, 35);
+	cout << "Queue's list after insert 35 after 30: ";
+	queue.Print();
+	
+	queue.UpdateItem(6, 55);
+	cout << "Queue's list after update 60 to 55: ";
+	queue.Print();
+
+	queue.Reverse();
+	cout << "Queue's list after reverse: ";
+	queue.Print();
+
+	queue.Clear();
+	cout << "Queue's list after clear: ";
+	queue.Print();
+
+	return 0;
+}
---
+++ level-1/QueueClass/clsQueue.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "..\P1-LinkedListClass\clsDoublyLinkedList.h"
+
+template <class T> class clsQueue {
+private:
+	clsDoublyLinkedList<T> _list;
+
+public:
+
+	void push(T data) {
+		_list.InsertAtEnd(data);
+	}
+
+	void pop() {
+		_list.DeleteFirstNode();
+	}
+
+	void Print() {
+		_list.PrintList();
+	}
+
+	int Size() {
+		return _list.Size();
+	}
+
+	T front() {
+		return _list.GetItem(0);
+	}
+
+	T back() {
+		return _list.GetItem(Size() - 1);
+	}
+
+	bool IsEmpty() {
+		return _list.IsEmpty();
+	}
+
+	T GetItem(int index) {
+		return _list.GetItem(index);
+	}
+
+	bool UpdateItem(int index, T newData) {
+		return _list.UpdateItem(index, newData);
+	}
+	
+	bool InsertAfter(int index, T data) {
+		return _list.InsertAfter(index, data);
+	}
+
+	void InsertAtFront(T data) {
+		_list.InsertAtBeginning(data);
+	}
+
+	void InsertAtBack(T data) {
+		_list.InsertAtEnd(data);
+	}
+
+	void Reverse() {
+		_list.Reveres();
+	}
+
+	void Clear() {
+		_list.Clear();
+	}
+
+};
